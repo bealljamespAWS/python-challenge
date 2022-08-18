7ca3e9b10dd973f9d0441d34adf390d2cb72db9b
bealljamespAWS <111200141+bealljamespAWS@users.noreply.github.com>
2022-08-17 18:45:13 -0700

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -29,25 +29,6 @@
 #   average_change = total_change / (months - 1) 
 
 
-
-
-# ....__annotations__
-
-
-
-# names = []
-# for _ in range(5):
-#     name = input("Please enter the name of someone you know. ")
-#     names.append(name)
-
-# lowercased = [name.lower() for name in names]
-# title_cased = [name.title() for name in lowercased]
-# invitations = [
-#     f"Dear {name}, please come to the wedding this Saturday!" for name in title_cased]
-
-# for invitation in invitations:
-#     print(invitation)
-
 import os
 import csv
 
@@ -71,7 +52,6 @@ with open(pybank_csv) as csvfile:
 
     csv_header = next(csvreader)
 
-    # loop through all of the rows in the csv
     for row in csvreader:
 
         total_profit_losses = total_profit_losses + int(row[1])
@@ -99,26 +79,17 @@ with open(output_file, "w") as csvfile:
     writer = csv.writer(csvfile)
     
     csvfile.write("Financial Analysis" + '\n')
-    #The total number of months included in the dataset
     csvfile.write("Total Months: " + str(months) + '\n')
     print("Total Months: " + str(months))
 
-    #The net total amount of "Profit/Losses" over the entire period
     csvfile.write("Total: $" + str(total_profit_losses) + '\n')
     print("Total: " + str(total_profit_losses))
 
-    #The changes in "Profit/Losses" over the entire period, and then the average of those changes
     csvfile.write("Average Change: $" + str(average_change) + '\n')
     print("Average Change: " + str(average_change))
 
-    #The greatest increase in profits (date and amount) over the entire period
     csvfile.write("Greatest Increase in Profits: $" + str(greatest_increase) + '\n')
     print("Greatest Increase in Profits: " + str(greatest_increase))
 
-    #The greatest decrease in profits (date and amount) over the entire period
     csvfile.write("Greatest Decrease in Profits: $" + str(greatest_decrease) + '\n')
     print("Greatest Decrease in Profits: " + str(greatest_decrease))
-
-#with open(output_file, 'w') as csvfile:
-    #csvwriter = csv.writer(csvfile)
-    #csv.writer.writerow("Total Months: " + str(months))
